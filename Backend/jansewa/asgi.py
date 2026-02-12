a3b47495719b990d9aed9f3da6c2ff6eec002fad
prashant.080bct49@acem.edu.np <prashant.080bct49@acem.edu.np>
2026-02-13 03:53:31 +0545

    chore(jansewa): Completed configurations

--- Backend/jansewa/asgi.py
+++ Backend/jansewa/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for jansewa project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
+# backend/jansewa/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jansewa.settings')
-
 application = get_asgi_application()
--- Backend/jansewa/urls.py
+++ Backend/jansewa/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for jansewa project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# backend/jansewa/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('apps.users.urls')),
+    path('api/v1/municipalities/', include('apps.municipalities.urls')),
+    path('api/v1/providers/', include('apps.providers.urls')),
+    path('api/v1/bookings/', include('apps.bookings.urls')),
+    path('api/v1/reviews/', include('apps.reviews.urls')),
+    path('api/v1/payments/', include('apps.payments.urls')),
+    path('api/v1/governance/', include('apps.governance.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
